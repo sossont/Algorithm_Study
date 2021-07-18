5bab84a9f75c8b543a1b1754fc62e6bd3baa1315
hwanu <sossont1@gmail.com>
2021-07-18 23:23:53 +0900

    21.07.18 알리알리알라셩

---
+++ 알리알리알라셩/동적 리스트, 스택, 큐, 덱/5397 키로거.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+#include <list>
+using namespace std;
+
+
+int main(){
+    int tc;
+    cin >> tc;
+    list<char>::iterator iter;
+    for(int t = 0; t<tc; t++) {
+        list<char> arr;
+        string str;
+        cin >> str;
+        list<char>::iterator cursor = arr.end();  // 커서 초기 위치.
+        for (int i = 0; i < str.length(); i++){
+            char judge = str[i];
+            if(judge == '<'){
+                if(cursor == arr.begin())
+                    continue;
+                cursor--;
+            }
+            else if(judge == '>')
+            {
+                if(cursor == arr.end())
+                    continue;
+                cursor++;
+            }
+            else if(judge == '-'){
+                if(cursor == arr.begin())
+                    continue;
+                cursor = arr.erase(--cursor);
+            }
+            else{
+                arr.insert(cursor, judge);
+            }
+        }
+
+        for(iter = arr.begin(); iter!=arr.end(); iter++)
+            cout << *iter;
+        cout << '\n';
+    }
+}
